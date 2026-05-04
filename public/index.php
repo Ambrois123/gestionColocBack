7f3325d6c304143b116d35b099e0743d6e2350fa
Ambroise Adanledji <adanledjiambroise@gmail.com>
2026-05-04 18:18:43 +0200

    Configuration de CORS dans index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+//CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+//Gestion des requêtes OPTIONS pour les pré-vols CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\controllers\AuthController;
